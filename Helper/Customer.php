c40b8f07915465fbdc2702e8a98bc10b11ab91e0
Yahyaoui Faouzi <faouzi.yahyaoui@lepape.com>
2021-07-25 18:40:56 +0100

    Customer Helper - add sequence

--- Helper/Customer.php
+++ Helper/Customer.php
@@ -6,17 +6,35 @@
 
 namespace Ideas2d\Helpers\Helper;
 
+use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 
 class Customer extends AbstractHelper
 {
     /**
+     * @var CustomerSession $customerSession
+     */
+    private CustomerSession $customerSession;
+
+    /**
      * @param Context $context
+     * @param CustomerSession $customerSession
      */
     public function __construct(
-        Context $context
+        Context $context,
+        CustomerSession $customerSession
     ) {
         parent::__construct($context);
+
+        $this->customerSession = $customerSession;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoggedIn(): bool
+    {
+        return $this->customerSession->isLoggedIn();
     }
 }
